5ff574c40397760ce74519fe6dd7af8276c2393e
ddddddwwwwiiiaap <dwiaprilyaanggoroputry02@gmail.com>
2023-05-13 18:44:40 +0700

    Adding id variable

--- lib/model/kategori_barang_model.dart
+++ lib/model/kategori_barang_model.dart
@@ -2,27 +2,33 @@
 import 'dart:convert';
 
 class KategoriBarangModel {
-  final String nama;
+  final int id;
+  String nama;
   KategoriBarangModel({
+    required this.id,
     required this.nama,
   });
 
   KategoriBarangModel copyWith({
+    int? id,
     String? nama,
   }) {
     return KategoriBarangModel(
+      id: id ?? this.id,
       nama: nama ?? this.nama,
     );
   }
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
+      'id_kategori_barang': id,
       'nama_kategori_barang': nama,
     };
   }
 
   factory KategoriBarangModel.fromMap(Map<String, dynamic> map) {
     return KategoriBarangModel(
+      id: map['id_kategori_barang'] as int,
       nama: map['nama_kategori_barang'] as String,
     );
   }
@@ -32,16 +38,17 @@ class KategoriBarangModel {
   factory KategoriBarangModel.fromJson(String source) => KategoriBarangModel.fromMap(json.decode(source) as Map<String, dynamic>);
 
   @override
-  String toString() => 'KategoriBarangModel(nama: $nama)';
+  String toString() => 'KategoriBarangModel(id: $id, nama: $nama)';
 
   @override
   bool operator ==(covariant KategoriBarangModel other) {
     if (identical(this, other)) return true;
   
     return 
+      other.id == id &&
       other.nama == nama;
   }
 
   @override
-  int get hashCode => nama.hashCode;
+  int get hashCode => id.hashCode ^ nama.hashCode;
 }
